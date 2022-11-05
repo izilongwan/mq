a3a0e3f1d8527640ad633aff70b518f8df3ae0dc
izilongwan <js.nb@qq.com>
2022-11-05 23:49:26 +0800

    add ttl 死信队列 延时队列

--- comsumer/src/main/java/com/consumer/listener/RabbitLisener.java
+++ comsumer/src/main/java/com/consumer/listener/RabbitLisener.java
@@ -1,7 +1,7 @@
 package com.consumer.listener;
 
-import java.util.Date;
-
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Argument;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
@@ -9,6 +9,7 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import com.common.entity.R;
+import com.rabbitmq.client.Channel;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,10 +22,70 @@ public class RabbitLisener {
         log.info("msg 1 ==> {}", msg);
     }
 
-    @RabbitListener(bindings = @QueueBinding(value = @Queue(name = "direct.2"), exchange = @Exchange(value = "direct"), key = {
-            "2", "0" }))
-    public void lisener2(R<String> msg) {
-        log.info("msg 2 ==> {}", msg);
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue(name = "direct.2"), exchange = @Exchange(value = "direct"), key = {
+                    "2", "0" })
+    })
+    public void lisener2(R<String> msg, Channel c, Message m) throws Exception {
+        long tag = m.getMessageProperties().getDeliveryTag();
+        log.info("2 tag ==> {} ==> {}", tag, msg);
+
+        // try {
+        // // log.info("{}", c);
+        // // log.info("{}", m);
+        // c.basicAck(tag, false);
+        // } catch (Exception e) {
+        // c.basicNack(tag, false, false);
+        // e.printStackTrace();
+        // }
+        Thread.sleep(1000 / 2);
+        // c.basicQos(2);
+        // c.basicConsume("direct.2", null);
+    }
+
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue(name = "direct.2"), exchange = @Exchange(value = "direct"), key = {
+                    "2", "0" })
+    })
+    public void lisener3(R<String> msg, Channel c, Message m) throws Exception {
+        long tag = m.getMessageProperties().getDeliveryTag();
+        log.info("3 tag ==> {} ==> {}", tag, msg);
+
+        // try {
+        // c.basicAck(tag, false);
+        // } catch (Exception e) {
+        // c.basicNack(tag, false, false);
+        // e.printStackTrace();
+        // }
+        Thread.sleep(1000);
+    }
+
+    /**
+     * 死信消息：
+     * 消息过期且未被消费
+     * 队列消息长度最大限制
+     * 消息拒收且未返回原队列
+     *
+     * 延迟队列：死信队列 + ttl
+     */
+    @RabbitListener(bindings = {
+            @QueueBinding(value = @Queue(name = "ttl.queue", arguments = {
+                    @Argument(name = "x-dead-letter-exchange", value = "direct"),
+                    @Argument(name = "x-dead-letter-routing-key", value = "1"),
+            }), exchange = @Exchange(value = "ttl.exchange"), key = {
+                    "2", "0" })
+    })
+    public void lisener4(R<String> msg, Channel c, Message m) throws Exception {
+        long tag = m.getMessageProperties().getDeliveryTag();
+        log.info("22 tag ==> {} ==> {}", tag, msg);
+
+        // try {
+        // c.basicAck(tag, false);
+        // } catch (Exception e) {
+        // c.basicNack(tag, false, false);
+        // e.printStackTrace();
+        // }
+        Thread.sleep(1000);
     }
 
 }
--- publish/src/main/java/com/publish/controller/UserController.java
+++ publish/src/main/java/com/publish/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.publish.controller;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.ReturnedMessage;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson2.JSON;
 import com.common.entity.R;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("p")
+@Slf4j
 public class UserController {
     @Resource
     RabbitTemplate rabbitTemplate;
+
+    @PostConstruct
+    public void name() {
+        // publish异常消息处理
+        // exchange处理模式
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setConfirmCallback((CorrelationData c, boolean b, String s) -> {
+            if (!b) {
+                log.info("{}", c);
+                log.info(s);
+            }
+        });
+
+        rabbitTemplate.setReturnsCallback((ReturnedMessage returned) -> {
+            log.info("{}", returned);
+        });
+    }
 
     @GetMapping("")
     public R<R<String>> user() {
@@ -37,14 +63,42 @@ public class UserController {
     }
 
     @GetMapping("direct/{id}")
-    public R<R<String>> id1(@PathVariable String id) {
+    public R<R<String>> id1(@PathVariable String id) throws Exception {
         R<String> v = new R<String>(id);
 
         String exchange = "direct";
         String key = id;
         R<String> rs = R.SUCCESS(id);
 
-        rabbitTemplate.convertAndSend(exchange, key, rs);
+        for (int i = 0; i < 20; i++) {
+            rabbitTemplate.convertAndSend(exchange, key, rs);
+        }
+
+        return R.SUCCESS(v);
+    }
+
+    @GetMapping("delay/{id}/{delay}")
+    public R<R<String>> id2(@PathVariable String id, @PathVariable String delay) throws Exception {
+        R<String> v = new R<String>(id);
+
+        String exchange = "ttl.exchange";
+        String key = id;
+        final R<String> rs = R.SUCCESS(id);
+
+        for (int i = 0; i < 20; i++) {
+            MessagePostProcessor messagePostProcessor = new MessagePostProcessor() {
+
+                @Override
+                public Message postProcessMessage(
+                        Message message) throws AmqpException {
+                    // 设置消息过期时间
+                    message.getMessageProperties().setExpiration(delay);
+                    return message;
+                }
+
+            };
+            rabbitTemplate.convertAndSend(exchange, key, rs, messagePostProcessor);
+        }
 
         return R.SUCCESS(v);
     }
